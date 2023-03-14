802715ea503a5e502619692caed3b22aa7363cbb
Shannon Melvin <Shannonmelvin88@gmail.com>
2023-03-14 19:06:52 -0400

    2nd commit

--- day1_hw.js
+++ day1_hw.js
@@ -2,13 +2,32 @@ let dog_string = "Hello Max, my name is Dog, and I have purple eyes!"
 let dog_names = ["Max","HAS","PuRple","dog"]
 
 function findWords(dog_string,dog_names){ 
+    // This function will loop through list and check if name in string
     for(i=0; i < dog_names.length; i++){
-        if(dog_names[i] == dog_string){
-            `Matched ${dog_name}`
+        let current_name = dog_names[i]
+        // declared current name as a variable 
+        // beginning of if statement
+        if(dog_string.toLowerCase().includes(current_name.toLowerCase())){
+            console.log(`Matched ${current_name}`)
         }else{
             console.log("No Match")
         }
     }
 
 }    
-findWords(dog_string,dog_names)
+// called func here
+findWords(dog_string,dog_names)   
+
+
+
+let arr =["Max","Baseball","Reboot","Goku","Trucks","Rodger"]
+// define function here
+function replaceEvens(arr){ 
+    // looping through the even indices in array
+    for(let i =0; i < arr.length; i+=2){
+        // this will replace every even index with 
+        arr.splice(i,1,"even index")
+    }
+    return arr
+}  
+console.log(replaceEvens(arr))
